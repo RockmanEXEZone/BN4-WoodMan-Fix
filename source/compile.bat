c50df4cd70edffd7bcb88d91946729038a7caf30
Prof. 9 <prof9@outlook.com>
2016-04-23 10:43:54 +0200

    Pause on errors

--- source/compile.bat
+++ source/compile.bat
@@ -1,7 +1,14 @@
-armips src.asm -equ ver bme.asm
-armips src.asm -equ ver bmj.asm
-armips src.asm -equ ver bmu.asm
-armips src.asm -equ ver rse.asm
-armips src.asm -equ ver rsj.asm
-armips src.asm -equ ver rsj1.asm
-armips src.asm -equ ver rsu.asm
+armips src.asm -equ ver "bme.asm"
+if errorlevel 1 pause
+armips src.asm -equ ver "bmj.asm"
+if errorlevel 1 pause
+armips src.asm -equ ver "bmu.asm"
+if errorlevel 1 pause
+armips src.asm -equ ver "rse.asm"
+if errorlevel 1 pause
+armips src.asm -equ ver "rsj.asm"
+if errorlevel 1 pause
+armips src.asm -equ ver "rsj1.asm"
+if errorlevel 1 pause
+armips src.asm -equ ver "rsu.asm"
+if errorlevel 1 pause
